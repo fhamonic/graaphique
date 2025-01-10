240765a806ba1f2d08e8caf381ecc3c2344e6feb
fhamonic <francois.hamonic@gmail.com>
2025-01-10 01:47:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Installer nodejs et npm :
+
+    sudo apt install nodejs npm
+
+puis installer express
+
+    sudo npm install express mustache-express echarts jquery
+
+
+et lancer le serveur avec :
+
+    node server.js
---
+++ server.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let express = require('express');
+let mustache = require('mustache-express');
+var path = require('path');
+let fs = require('fs');
+
+let app = express();
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './views');
+
+app.set('view engine', 'html');
+app.use("/", express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000, () => {
+    console.log('Listening http://localhost:3000 !');
+});
